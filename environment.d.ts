d5e3afa0784ddfc357d8236ee668c5950f9e68dd
lzear <antoine.clausse@gmail.com>
2025-01-26 11:47:38 +0100

    chore: remove types modules from environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -5,28 +5,7 @@ declare module '@eslint-community/eslint-plugin-eslint-comments' {
   export = plugin
 }
 
-declare module '@vitest/eslint-plugin' {
-  import type { ESLint } from 'eslint'
-
-  let plugin: ESLint.Plugin
-  export = plugin
-}
-
-declare module 'eslint-plugin-import-x' {
-  import type { ESLint } from 'eslint'
-
-  let plugin: ESLint.Plugin
-  export = plugin
-}
-
 declare module 'eslint-plugin-jsx-a11y' {
-  import type { ESLint } from 'eslint'
-
-  let plugin: ESLint.Plugin
-  export = plugin
-}
-
-declare module 'eslint-plugin-package-json' {
   import type { ESLint } from 'eslint'
 
   let plugin: ESLint.Plugin
@@ -54,13 +33,6 @@ declare module 'eslint-plugin-promise' {
   export = plugin
 }
 
-declare module 'eslint-plugin-react' {
-  import type { ESLint } from 'eslint'
-
-  let plugin: ESLint.Plugin
-  export = plugin
-}
-
 declare module 'eslint-plugin-react-compiler' {
   import type { ESLint } from 'eslint'
 
@@ -80,15 +52,4 @@ declare module 'eslint-plugin-react-perf' {
 
   let plugin: ESLint.Plugin
   export = plugin
-}
-
-declare module 'typescript-eslint' {
-  import type { ESLint } from 'eslint'
-
-  export let plugin: ESLint.Plugin
-  export let parser: ESLint.Parser
-  export = {
-    plugin,
-    parser,
-  }
 }
